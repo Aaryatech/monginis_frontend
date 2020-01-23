f45436eab7e6a922dcf7aa298a8375790eab0bf6
ats-12 <ats-12@ats12>
2020-01-23 12:14:35 +0530

    //Mahesh

--- src/main/java/com/monginis/ops/constant/Constant.java
+++ src/main/java/com/monginis/ops/constant/Constant.java
@@ -1,33 +1,35 @@
 package com.monginis.ops.constant;
 
-import java.io.File;
-
 public class Constant {
 
-	 public static final String URL = "http://132.148.151.41:8080/webapi/";//http://mongiwebapi.ap-south-1.elasticbeanstalk.com/
-	  //public static final String URL = "http://localhost:8099/";
-	 //public static final String URL = "http://mongiwebapi.ap-south-1.elasticbeanstalk.com/";
+	 public static final String URL = "http://107.180.95.11:8080/webapi/";//http://mongiwebapi.ap-south-1.elasticbeanstalk.com/
+//		public static final String URL = "http://localhost:8096/";
+//	 public static final String URL = "http://mongiwebapi.ap-south-1.elasticbeanstalk.com/";
 
-	public static final String ITEM_IMAGE_URL = "http://132.148.151.41:8080/uploads/ITEM/";
+	public static final String ITEM_IMAGE_URL = "http://107.180.95.11:8080/uploads/ITEM/";
 
-	public static final String SP_CAKE_FOLDER="http://132.148.151.41:8080/uploads/SPCAKE/";
+	public static final String SP_CAKE_FOLDER="http://107.180.95.11:8080/uploads/SPCAKE/";
 	
-	public static final String SPCAKE_IMAGE_URL = "http://132.148.151.41:8080/uploads/MSPCAKE/";
+	public static final String SPCAKE_IMAGE_URL = "http://107.180.95.11:8080/uploads/MSPCAKE/";
 
 	
-	public static final String PHOTO_CAKE_URL="http://132.148.151.41:8080/uploads/CUSTCHOICEPHOTOCAKE/";
+	public static final String PHOTO_CAKE_URL="http://107.180.95.11:8080/uploads/CUSTCHOICEPHOTOCAKE/";
 
-	public static final String FR_IMAGE_URL="http://132.148.151.41:8080/uploads/FR/";
+	public static final String FR_IMAGE_URL="http://107.180.95.11:8080/uploads/FR/";
 	
-	public static final String GVN_IMAGE_URL="http://132.148.151.41:8080/uploads/GVN/";
+	public static final String GVN_IMAGE_URL="http://107.180.95.11:8080/uploads/GVN/";
 
 	//public static final String ReportURL ="http://localhost:8986/ops/";
-	public static final String ReportURL ="http://132.148.151.41:8080/ops/";
-	public static final String MESSAGE_IMAGE_URL="http://132.148.151.41:8080/uploads/MSG/";
+	public static final String ReportURL ="http://107.180.95.11:8080/ops/";
+	public static final String MESSAGE_IMAGE_URL="http://107.180.95.11:8080/uploads/MSG/";
 
-	public static final String LOGIS_BILL_URL ="http://132.148.151.41:8080/uploads/MSPCAKE/";
+	public static final String LOGIS_BILL_URL ="http://107.180.95.11:8080/uploads/MSPCAKE/";
 
 	public static final String REPORT_SAVE = "/opt/tomcat-latest/webapps/uploads/Report.pdf";
+
+	public static final String FACTORYNAME = "";
+
+	public static final String FACTORYADDRESS = "";
 	
 
 	public static Integer dayOfMonthEnd=1;
@@ -44,8 +46,6 @@ public class Constant {
 	
 	public static int GVN_IMAGE_TYPE=6;
 
-	
-	
-	
+	public static float sale_per=1.25f;
 
 }
